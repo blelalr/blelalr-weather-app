dd307b32c363c4be5fad78b5d79799e02bb289d8
Eros <blelarg@gmail.com>
2014-08-16 13:27:10 +0800

    宣告變數

--- WeaterAPP/ViewController.swift
+++ WeaterAPP/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        var helloSwift:String = "Hello Swift"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
